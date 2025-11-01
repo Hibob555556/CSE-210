36724b20914a9214bcc0b2b4d878c7d7923b1441
Cayden Lunt <cayden.lunt@gmail.com>
2025-10-31 19:11:06 -0600

    completed exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int num = -9999;
+        List<int> numbers = [];
+        while (num != 0)
+        {
+            Console.Write("Enter number: ");
+            try
+            {
+                num = int.Parse(Console.ReadLine());
+                if (num != 0)
+                    numbers.Add(num);
+            }
+            catch
+            {
+                Console.WriteLine("Please Input A Valid Whole Number.");
+            }
+        }
+        int sum = numbers.Sum();
+        double avg = numbers.Average();
+        int lrg = numbers.Max();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {avg:F2}");
+        Console.WriteLine($"The largest number is {lrg}");
     }
 }
