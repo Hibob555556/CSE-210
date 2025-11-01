1cf5d53f559f51fbd5a5e016f5ad3d85e0b9de03
Cayden Lunt <cayden.lunt@gmail.com>
2025-10-31 18:17:14 -0600

    added exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        int m_number;
+        int g_number;
+        bool guessed = false;
+
+        Random rand = new();
+        m_number = rand.Next(1, 101);
+
+        int count = 0;
+        while (!guessed)
+        {
+            count++;
+            Console.Write("What is your guess? ");
+            try
+            {
+                g_number = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Try Again");
+                continue;
+            }
+
+            if (m_number == g_number)
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"Guesses: {count}.");
+                Console.Write("Would you like to play again (0 = no, 1 = yes)? ");
+                int again = int.Parse(Console.ReadLine());
+                if (again != 1)
+                {
+                    guessed = true;
+                    Console.WriteLine("Goodbye!");
+                }
+                else
+                {
+                    m_number = rand.Next(1, 101);
+                    count = 0;
+                }
+            }
+            else if (g_number > m_number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+        }
     }
 }
