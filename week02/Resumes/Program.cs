6d261905a461ac254e2a99838b2e4dd6334b1a3d
Cayden Lunt <cayden.lunt@gmail.com>
2025-11-08 23:24:09 -0700

    completed Resume program

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using Resumes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job = new Job();
+        job._company = "Campbell Scientific";
+        job._jobTitle = "Product Test Engineer";
+        job._startYear = 2024;
+        job._endYear = 2030;
+
+        Job job1 = new Job();
+        job1._company = "Space Dynamics Laboratory";
+        job1._jobTitle = "Student Engineering Assistant";
+        job1._startYear = 2022;
+        job1._endYear = 2024;
+
+        Resume resume = new Resume();
+        resume._name = "Cayden Lunt";
+        resume._jobs = [job, job1];
+
+        resume.Display();
     }
 }
